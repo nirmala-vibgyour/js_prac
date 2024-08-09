5b99d56026e4812e4d9888f0331f970370918d65
nirmala-vibgyor <nirmalawrk@gmail.com>
2024-08-09 11:26:29 +0530

    Creation of string object in some diff ways.

--- strings/str_obj.js
+++ strings/str_obj.js
@@ -1,2 +1,18 @@
+// Creating using String() and new.
 var sstring = new String()
 console.log(typeof(sstring))
+
+// Creating using the method.
+var strobj = new String("test")
+console.log(strobj)
+console.log(typeof(strobj))
+
+// Creating as a literal.
+var strobj1 = {
+    color: "red",
+    month: "oct",
+    code: 34,          // Need to check why it is accepting the number data type.
+    name: "robert"
+}
+console.log(strobj1)
+console.log(typeof(strobj1['code']))
