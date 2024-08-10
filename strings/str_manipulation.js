df5546287d910c16b0259a433f89bc5a6060a9a7
nirmala-vibgyor <nirmalawrk@gmail.com>
2024-08-10 12:28:59 +0530

    Added few more string methods

--- strings/str_manipulation.js
+++ strings/str_manipulation.js
@@ -19,3 +19,33 @@ console.log("Panmonnnpmon".indexOf("mon"))
 // using last indexOf
 console.log("Panmonnnpmon".lastIndexOf("mon"))
 // using match
+// take as argument: "string" or "regular expression"
+// output an array containing all the strings with matched pattern"
+// but, here it seems to give only the first occu
+console.log("The user with the username cannot be find as the user in our user database".match("us"))
+// using replace
+// but, here it seems to give only the first occu
+console.log("The user with the username cannot be find as the user in our user database".replace("user","hello"))
+// using slice
+// The method, as revealed by its signature, replace(start, [end]);,
+// requires a starting index and an optional ending index.
+index1 = "JavaScript is used in web development".indexOf("w")
+index2 = "JavaScript is used in web development".indexOf("m")
+console.log("JavaScript is used in web development.".slice(index1))
+console.log("JavaScript is used in web development.".slice(index1, index2))
+// using substr
+// The required parameter, start, signifies where the substring to extract begins. This
+// value can be followed by an optional number of characters to include in the returned substring.
+console.log("JavaSript is used in web development.".substr(12, "JavaSript is used in web development.".length))
+// using split
+// return an array of splited parts
+console.log("string:numbers:boolean:null:undefined".split(":"))
+// using toUpperCase
+console.log("JavaScript".toUpperCase())
+// using.tolowerCase
+console.log("JavaScript".toLowerCase())
+// comparison
+console.log("JS" == "JS".toLowerCase())
+
+
+
