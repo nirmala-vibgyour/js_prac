5975a782746e177edc3854da928f36cffd6dac72
nirmala-vibgyour <nirmalawrk@gmail.com>
2024-09-14 22:01:46 +0530

    added examples of functions, arrays and objects

--- functions/funcTypes.js
+++ functions/funcTypes.js
@@ -18,7 +18,7 @@ function funcFactory(value) {
     };
 }
 const func = funcFactory('abc');
-assert.equal(func(), 'abc'); 
+// assert.equal(func(), 'abc'); 
 // here, the variable func has become a variable and can access the free variable 'value'
 // func is having the value cause it is what has been returned, func is the closure
 // func is a closure because it has access to the value parameter of funcFactory,
@@ -27,3 +27,92 @@ assert.equal(func(), 'abc');
 // In simpler terms, a closure allows a function to access variables from its outer (enclosing) 
 // scope even after that outer function has finished executing.
 
+// function expression
+const adD = function(x) {
+  return x+2;
+};
+
+console.log(adD(3));
+
+let sub = function(y) {
+  return 10 - y;
+};
+let sub1 = sub(2);
+let sub2 = sub(5);
+
+console.log(sub1, sub2);
+
+var multiply = function(z) {
+  return z*10;
+};
+
+let m1 = multiply(2);
+let m2 = multiply(5);
+
+console.log(m1, m2);
+
+// arrow function
+
+const power = (base, exponent) => {       // Here, power couldn't be assigned to any other value.
+  let power;
+  power = base**exponent;
+  return power;
+};
+let p1 = power(5,2);
+p1 = power(4,2);
+
+let p2 = power(3,2);
+console.log(p1, p2);
+
+const square = (x) => { return x*x};
+
+console.log(square(9));
+
+const square2 = x => x*x;
+console.log(square2(10));
+
+const greet = () => {
+  return "arrow function";
+};
+
+console.log(greet());
+
+// optional arguments
+
+const equ = (marks, def=10) => {
+  return marks+def;
+};
+
+console.log(equ(50));
+console.log(equ(50, 20));
+
+// closure
+const rearrange = function(marks) {
+  return carryOver => marks+carryOver;
+};
+
+let r1 = rearrange(50);  // marks equ 50
+console.log(r1(20));      // carryOver equ 20
+
+// closure: normal function statment
+
+function calculation(marks, balance) {
+  return function(reduction) {
+    return ((marks + balance) - reduction);
+  };
+}
+
+let calc1 = calculation(55, 15);
+console.log(calc1(10));
+
+// recursion
+function factorial(n) {
+  if (n !== 1) {
+    return n*(factorial(n-1))
+  }
+  else {
+    return 1;
+  }
+}
+
+console.log(factorial(5));
--- objects/creating_obj.js
+++ objects/creating_obj.js
@@ -14,3 +14,35 @@ days = ["sun", "mon", "tue", "wed", "thurs", "fri", "sat"]
 console.log(days)
 
 // just to show when we aare initializing and when we are making literals.
+
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+console.log(listOfNumbers[2-1]);
+console.log(listOfNumbers.length);
+
+// to access properties use: dot & [] notation
+
+let day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running"]
+};
+console.log(day1["squirrel"]);
+console.log(day1.squirrel);
+
+// array of objects
+let journal = [
+    {events: ["work", "touched tree", "pizza",
+    "running", "television"],
+    squirrel: false},
+    {events: ["work", "ice cream", "cauliflower",
+    "lasagna", "touched tree", "brushed teeth"],
+    squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts",
+    "beer"],
+    squirrel: true},
+    /* and so on... */
+];
+
+console.log(journal);
+
+// objects are mutable: their values can be changed at any time
